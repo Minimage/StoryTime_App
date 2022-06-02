67afae7a50c99c2a3c2577a70b4afcdb018ede92
Minimage <seancampbell914@gmail.com>
2022-06-02 15:56:14 +0000

    added comments

--- src/front/js/pages/user.js
+++ src/front/js/pages/user.js
@@ -5,6 +5,10 @@ import { Login } from "./login";
 
 export const User = () => {
   const { auth, setAuth } = useContext(userContext);
+
+  //Checks to see if the user is autherized (logged in) and if they are not then it will redirect to login page
+  //This will prevent someone from just trying to access the user page or content they are not autherized to
+  //access by simply typing in the url for the page
   if (!auth) {
     return <Redirect to={"/Login"} />;
   }
