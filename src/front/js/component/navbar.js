bd082eec666c10970713a520861c7f1c4c80dc3a
Minimage <seancampbell914@gmail.com>
2022-06-04 23:21:55 +0000

    Removed the login button from the home page and added it to the navbar so it can be accessed from anywhere
    Made it so that the login button will disapper once the user is logged in
    
    **Next Steps**
    Upcoming task is to make it so when the user logs in that the profile picture of the user will go where the login button was (good idea for desktop, idk about mobile)

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { Link } from "react-router-dom";
+import "../../styles/styles.css";
+import { userContext } from "../pages/global_context";
 
 export const Navbar = () => {
+  const { auth, setAuth } = useContext(userContext);
   return (
     <nav className="navbar navbar-light bg-light">
       <div className="container">
@@ -9,10 +12,14 @@ export const Navbar = () => {
           <span className="navbar-brand mb-0 h1">Home</span>
         </Link>
         <div className="ml-auto">
-          <Link to="/demo">
-            <button className="btn btn-primary">
-              Check the Context in action
-            </button>
+          <Link to="/Login">
+            <span
+              className={
+                auth === true ? "hide" : "show" + " btn btn-primary texter"
+              }
+            >
+              Login
+            </span>
           </Link>
         </div>
       </div>
