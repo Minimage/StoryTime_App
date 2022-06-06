aca0ec7548c0fabe82666c838e0b6e841e282b04
Minimage <seancampbell914@gmail.com>
2022-06-06 01:50:50 +0000

    Created a copy of element home element to test how to make it render only on view

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -48,6 +48,39 @@ export const Home = () => {
               </div>
             </div>
           </Fade>
+
+          {/* Split */}
+
+          <Fade in>
+            <div class="row d-flex align-items-center">
+              <div
+                class="col-lg-6 py-5 py-lg-0 order-2 order-lg-1 aos-init aos-animate"
+                data-aos="fade-right"
+              >
+                <Transform enterTransform="translateX(40px)" in>
+                  <h1>Ever had a hard time learning something new?</h1>
+                  <h2>
+                    Story Time is a fun and innovative way to help trigger yours
+                    <br></br>Memory to request words and everything you've
+                    learned
+                  </h2>
+                  <Link to="/signup">
+                    <button type="button" class="btn btn-warning">
+                      Get Started
+                    </button>
+                  </Link>
+                </Transform>
+              </div>
+              <div
+                class="col-lg-6 order-1 order-lg-2 hero-img aos-init aos-animate"
+                data-aos="fade-left"
+              >
+                <Transform enterTransform="translateX(-30px)" in>
+                  <img src={image} class="img-fluid fade in" alt="" />
+                </Transform>
+              </div>
+            </div>
+          </Fade>
         </div>
       </div>
     );
