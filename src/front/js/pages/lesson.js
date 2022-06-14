f7e32cea3fb6f9066e04ede08bf2da0581ea8555
Minimage <seancampbell914@gmail.com>
2022-06-14 04:51:39 +0000

    Fixed issue with page not loading, issue was due to an extra ")" on the appContext.js file

--- src/front/js/pages/lesson.js
+++ src/front/js/pages/lesson.js
@@ -1,17 +1,13 @@
 import React, { useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
-import LessonComponent from '../component/display_lesson';
+import LessonComponent from "../component/display_lesson";
 
- const Lesson = () => {
-    const { actions, store } = useContext(Context);
-    console.log(store.current_lesson)
-    useEffect(() => {
-        actions.initializeLesson();
-    }, [])
+export const Lesson = () => {
+  const { actions, store } = useContext(Context);
+  console.log(store.current_lesson);
+  useEffect(() => {
+    actions.initializeLesson();
+  }, []);
 
-    return (
-        <LessonComponent />
-    )
-}
-
-export default Lesson
+  return <LessonComponent />;
+};
--- src/front/js/store/appContext.js
+++ src/front/js/store/appContext.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import getState from "./flux.js";
-
 // Don't change, here is where we initialize our context, by default it's just going to be null.
 export const Context = React.createContext(null);
 
@@ -29,21 +28,21 @@ const injectContext = (PassedComponent) => {
        * store, instead use actions, like this:
        **/
       // state.actions.getMessage(); // <---- calling this function from the flux.js actions
-      // //____________________________________________________________________________________________________________			
-      state.actions.getAudio("word")
+      // //____________________________________________________________________________________________________________
+      state.actions.getAudio("word");
       //____________________________________________________________________________________________________________
+    }, []);
 
-      // The initial value for the context is not null anymore, but the current state of this component,
-      // the context will now have a getStore, getActions and setStore functions available, because they were declared
-      // on the state of this component
-      return (
-        <Context.Provider value={state}>
-          <PassedComponent {...props} />
-        </Context.Provider>
-      );
-    });
-    return StoreWrapper;
+    // The initial value for the context is not null anymore, but the current state of this component,
+    // the context will now have a getStore, getActions and setStore functions available, because they were declared
+    // on the state of this component
+    return (
+      <Context.Provider value={state}>
+        <PassedComponent {...props} />
+      </Context.Provider>
+    );
   };
-}
+  return StoreWrapper;
+};
 
 export default injectContext;
