3b5b4f3da6594955d6931144b440afbc36b8f151
Minimage <seancampbell914@gmail.com>
2022-07-12 23:01:54 +0000

    Fixed transition issue

--- src/front/js/pages/about_us.js
+++ src/front/js/pages/about_us.js
@@ -1,5 +1,6 @@
 import React, { useState, useContext, useEffect } from "react";
 // import Hangmen from "../component/Hangmen.js";
+import { Transform } from "react-animation-components";
 import "../../styles/about_us.css";
 
 // import { motion } from "framer-motion";
@@ -26,67 +27,113 @@ export const AboutUs = () => {
           onClick={toggleOpen}
         >
           <div className="slide">
-            <img id="picture"
-              className={state ? "active" : ""}
-              src="https://ca.slack-edge.com/T0BFXMWMV-U03538G6V9D-ac975af7b40f-512"
-            />
+            <Transform exitTransform="translateX(-1000px)" in>
+              <img
+                id="picture"
+                className={state ? "active" : ""}
+                src="https://ca.slack-edge.com/T0BFXMWMV-U03538G6V9D-ac975af7b40f-512"
+              />
+            </Transform>
 
-            <div className={state ? "left active" : "left"}>
-              <h1 className="name">Doug Montas</h1>
-              <h5>Coder professional</h5>
+            <Transform
+              enterTransform="translateX(0px)"
+              exitTransform="translateX(1000px)"
+              in
+            >
+              <div className={state ? "left active" : "left"}>
+                <h1 className="name">Doug Montas</h1>
+                <h5>Coder professional</h5>
 
-              <p className="text">
-                a Miami native. Doug is a future Blockchain specialist and
-                coding expert.
-              </p>
-              <h5><a href="https://github.com/DougMontas">Github</a> - <a href="https://www.linkedin.com/in/futureitprofessional/">LinkedIn</a></h5>
-            </div>
+                <p className="text">
+                  a Miami native. Doug is a future Blockchain specialist and
+                  coding expert.
+                </p>
+                <h5>
+                  <a href="https://github.com/DougMontas">Github</a> -{" "}
+                  <a href="https://www.linkedin.com/in/futureitprofessional/">
+                    LinkedIn
+                  </a>
+                </h5>
+              </div>
+            </Transform>
           </div>
           <div className="slide">
-            <img id="picture"
-              className={state ? "active" : ""}
-              src="https://ca.slack-edge.com/T0BFXMWMV-U033E6TK4QK-76ec8fa3b0b4-512"
-            />
+            <Transform exitTransform="translateX(-1000px)" in>
+              <img
+                id="picture"
+                className={state ? "active" : ""}
+                src="https://ca.slack-edge.com/T0BFXMWMV-U033E6TK4QK-76ec8fa3b0b4-512"
+              />
+            </Transform>
 
-            <div className={state ? "left active" : "left"}>
-              <h1 className="name">Sean Campbell</h1>
-              <h5>Coder professional</h5>
-              <p className="text">
-                a New York native. Sean is a musician with aspirations of being
-                a Python expert{" "}
-              </p>
-              <h5><a href="https://github.com/Minimage">Github</a> - <a href="https://www.linkedin.com/in/sean-campbell-cpht-437aa780/">LinkedIn</a></h5>
-            </div>
+            <Transform
+              enterTransform="translateX(0px)"
+              exitTransform="translateX(1000px)"
+              in
+            >
+              <div className={state ? "left active" : "left"}>
+                <h1 className="name">Sean Campbell</h1>
+                <h5>Coder professional</h5>
+                <p className="text">
+                  a New York native. Sean is a musician with aspirations of
+                  being a Python expert{" "}
+                </p>
+                <h5>
+                  <a href="https://github.com/Minimage">Github</a> -{" "}
+                  <a href="https://www.linkedin.com/in/sean-campbell-cpht-437aa780/">
+                    LinkedIn
+                  </a>
+                </h5>
+              </div>
+            </Transform>
           </div>
           <div className="slide">
-            <img id="picture"
-              className={state ? "active" : ""}
-              src="https://ca.slack-edge.com/T0BFXMWMV-U032MF8R23Z-e5010381314f-512"
-            />
+            <Transform exitTransform="translateX(-1000px)" in>
+              <img
+                id="picture"
+                className={state ? "active" : ""}
+                src="https://ca.slack-edge.com/T0BFXMWMV-U032MF8R23Z-e5010381314f-512"
+              />
+            </Transform>
 
-            <div className={state ? "left active" : "left"}>
-              <h1 className="name">Alan Martinez</h1>
-              <h5>Coder professional</h5>
-              <p className="text">
-                a Texas native, Alan is a gamer with a dream of being a coder
-                for google.{" "}
-              </p>
-            </div>
+            <Transform
+              enterTransform="translateX(0px)"
+              exitTransform="translateX(1000px)"
+              in
+            >
+              <div className={state ? "left active" : "left"}>
+                <h1 className="name">Alan Martinez</h1>
+                <h5>Coder professional</h5>
+                <p className="text">
+                  a Texas native, Alan is a gamer with a dream of being a coder
+                  for google.{" "}
+                </p>
+              </div>
+            </Transform>
           </div>
           <div className="slide">
-            <img id="picture"
-              className={state ? "active" : ""}
-              src="https://ca.slack-edge.com/T0BFXMWMV-U02FKP23DEJ-275306524d96-512"
-            />
+            <Transform exitTransform="translateX(-1000px)" in>
+              <img
+                id="picture"
+                className={state ? "active" : ""}
+                src="https://ca.slack-edge.com/T0BFXMWMV-U02FKP23DEJ-275306524d96-512"
+              />
+            </Transform>
 
-            <div className={state ? "left active" : "left"}>
-              <h1 className="name">Nick Sisneros</h1>
-              <h5>Coder professional</h5>
-              <p className="text">
-                A Colorado native, Nick is based in Shanghai and has dreams of
-                making video games.{" "}
-              </p>
-            </div>
+            <Transform
+              enterTransform="translateX(0px)"
+              exitTransform="translateX(1000px)"
+              in
+            >
+              <div className={state ? "left active" : "left"}>
+                <h1 className="name">Nick Sisneros</h1>
+                <h5>Coder professional</h5>
+                <p className="text">
+                  A Colorado native, Nick is based in Shanghai and has dreams of
+                  making video games.{" "}
+                </p>
+              </div>
+            </Transform>
           </div>
         </div>
       </div>
