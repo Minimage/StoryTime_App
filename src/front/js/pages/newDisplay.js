b4dffe3f8280d0770ae65d872c88b386d07a3120
Minimage <seancampbell914@gmail.com>
2022-07-19 18:50:38 +0000

    newDisplay moved to pages

---
+++ src/front/js/pages/newDisplay.js
@@ -0,0 +1,8 @@
+import { React, useState, useEffect } from "react";
+
+export const newDisplay = () => {
+  const [answer, setAnswer] = useState([]);
+  const [option, setOption] = useState([]);
+
+  return <div className="box"></div>;
+};
