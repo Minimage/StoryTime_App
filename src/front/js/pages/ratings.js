5c6a19974005c8a48572c73ffae175845791fcb9
Minimage <seancampbell914@gmail.com>
2022-07-20 00:34:39 +0000

    add comment

--- src/front/js/pages/ratings.js
+++ src/front/js/pages/ratings.js
@@ -1,33 +1,35 @@
 import React from "react";
-import  Rate  from "../component/rate.js"
+import Rate from "../component/rate.js";
 import { useState, useMemo } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "../../styles/ratings.css"
+import "../../styles/ratings.css";
 
-
+// comment
 
 export const Ratings = () => {
-    const [rating, setRating] = useState(0)
-    return (
-        <div className="background_ratings">
-        <div className="Rating_Title">
-            <h1> CONGRATULATIONS CHALLENGES COMPLETED!</h1>
-            <h3>GET READY FOR THE NEXT STORIES </h3>
-            <div>
-               
-                <Rate rating={rating} onRating={(rate) => setRating(rate)} count={5} />
-              
-                <h3 className="rating_count">Rating - {rating}</h3>
-                
-                <h1 className="rate_question">Rate us <br/>Share Your Achivements with Story Time</h1>
-            </div>
-            
+  const [rating, setRating] = useState(0);
+  return (
+    <div className="background_ratings">
+      <div className="Rating_Title">
+        <h1> CONGRATULATIONS CHALLENGES COMPLETED!</h1>
+        <h3>GET READY FOR THE NEXT STORIES </h3>
+        <div>
+          <Rate
+            rating={rating}
+            onRating={(rate) => setRating(rate)}
+            count={5}
+          />
+
+          <h3 className="rating_count">Rating - {rating}</h3>
+
+          <h1 className="rate_question">
+            Rate us <br />
+            Share Your Achivements with Story Time
+          </h1>
         </div>
-        </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Ratings
-
-
-
+export default Ratings;
